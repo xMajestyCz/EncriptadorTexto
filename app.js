ae3f6e95bfaabff38e6649c60cd18f10e4768900
Andres Henao <henaoandres2405@gmail.com>
2024-08-05 21:46:19 -0500

    modificando el responsive y terminando el codigo js

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 let vocales = ['e','i','a','o','u'];
 let encriptacion = ['enter','imes','ai','ober','ufat'];
 
+function quitarAparecer(a,b){
+    a.forEach(element => {
+        element.style.display = "block";
+    });
+    b.forEach(element => {
+        element.style.display = "none";
+    });
+}
 function recorrer(a,b){
     let texto = document.querySelector('.texto').value;
-    let img = document.querySelector('.img');
-    let sinTexto = document.querySelector('.mensaje-no-encontrado');
-    if(texto == null){
-
+    let nuevoTexto = document.querySelector('.nuevo-texto');
+    let quitarImg = document.querySelectorAll('.quitar');
+    let aparecerTexto = document.querySelectorAll('.aparecer');
+    if(texto == ''){
+        quitarAparecer(quitarImg,aparecerTexto);
     }
     else{
-        img.setAttribute('disabled');
-        sinTexto.setAttribute('disabled');
         for(let i = 0; i < a.length; i++){
             texto = texto.replaceAll(a[i], b[i]);
         }
+        quitarAparecer(aparecerTexto,quitarImg);
     }
-    
+    nuevoTexto.value = texto;
+}
+function quitarImg(){
+    if(window.innerWidth < 770){
+        let quitarImg = document.querySelector('.img');
+        quitarImg.style.display = "none";
+    }
 }
 function encriptar(){
     recorrer(vocales,encriptacion);
+    quitarImg();
 }
 function desencriptar(){
     recorrer(encriptacion,vocales);
+    quitarImg();
+}
+async function copiar() {
+    var textoCopiado = document.querySelector(".nuevo-texto").value;
+    try {
+        await navigator.clipboard.writeText(textoCopiado);
+        alert("Texto copiado!");
+    } catch (err) {
+        alert("Error al copiar el texto");
+    }
 }
